cdb10ca86d9b8bd7a94111928c2ecc70364ce29e
Diwakar <raikwarempire001@gmail.com>
2023-09-17 09:29:11 +0530

    size initialized

--- src/electricity/billing/system/Splash.java
+++ src/electricity/billing/system/Splash.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package electricity.billing.system;
 
-/**
- *
- * @author Diwakar
- */
-public class Splash {
-    
+package electricity.billing.system;
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame{
+    Splash(){
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/elect.jpg"));
+        Image i2= i1.getImage().getScaledInstance(730,550,Image.SCALE_DEFAULT);
+        ImageIcon i3= new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        add(image);
+        
+        setSize(730,550);
+        setLocation(500,200);
+        setVisible(true);
+    }
+
+    public static void main(String[]args){
+        new Splash();
+    }
 }
